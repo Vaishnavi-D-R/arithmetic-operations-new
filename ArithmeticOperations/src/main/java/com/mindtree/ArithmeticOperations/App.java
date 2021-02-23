cff18d791d786e5e491163ac7e574982a125debb
Vaishnavi-D-R <vaishnavidr2000@gmail.com>
2021-02-23 14:06:39 +0530

    first commit

---
+++ ArithmeticOperations/src/main/java/com/mindtree/ArithmeticOperations/App.java
@@ -0,0 +1,12 @@
+package com.mindtree.ArithmeticOperations;
+
+public class App {
+	public static void main(String[] args) {
+		int number1 = 10;
+		int number2 = 5;
+		int sum = number1 + number2;
+		int difference = number1 - number2;
+		System.out.println("Sum is " + sum);
+		System.out.println("Difference is " + difference);
+	}
+}
