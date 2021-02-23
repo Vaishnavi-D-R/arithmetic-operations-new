62d066a88c5445729009f80b9cf795b640dbf7e6
Vaishnavi-D-R <78353427+Vaishnavi-D-R@users.noreply.github.com>
2021-02-23 14:49:32 +0530

    second commit

--- ArithmeticOperations/src/main/java/com/mindtree/ArithmeticOperations/App.java
+++ ArithmeticOperations/src/main/java/com/mindtree/ArithmeticOperations/App.java
@@ -1,12 +1,19 @@
 package com.mindtree.ArithmeticOperations;
 
 public class App {
-	public static void main(String[] args) {
-		int number1 = 10;
-		int number2 = 5;
-		int sum = number1 + number2;
-		int difference = number1 - number2;
-		System.out.println("Sum is " + sum);
-		System.out.println("Difference is " + difference);
+	public int add(int a, int b) {
+		return a+b;
+	}
+
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+
+	public int divide(int a, int b) {
+		return a/b;
 	}
 }
